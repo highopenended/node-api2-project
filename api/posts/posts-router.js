11dc3c205ecc3673a6c1dd3a27c6571eadc0b0dc
Matt Glenn <highopenended@gmail.com>
2024-11-08 10:27:04 -0500

    Connected posts router

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,17 @@
 // implement your posts router here
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.status(200).send('hello from the GET /posts endpoint');
+});
+
+router.get('/:id', (req, res) => {
+    res.status(200).send(`hello from the GET /posts/${req.params.id} endpoint`);
+});
+
+router.post('/', (req, res) => {
+    res.status(200).send('hello from the POST /posts endpoint');
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,16 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Welcome to the API!');
+});
+
+const postsRoutes = require('./posts/posts-router');
+
+// server.use('/', defaultRoute);
+server.use('/posts', postsRoutes);
+
+module.exports = server;
+
